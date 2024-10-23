c843350c0d710aa96766492a8e8dbc760863ad88
Elysia <c.elysia@foxmail.com>
2024-10-23 16:59:32 +0800

    Added qml logger

--- easylogger/qml_plugin/LingmoLogger.h
+++ easylogger/qml_plugin/LingmoLogger.h
@@ -1,14 +1,41 @@
 #pragma once
 
-#include "elog.h"
+#include "singleton.h"
+#include "stdafx.h"
 
 #include <QObject>
-#include <qobjectdefs.h>
+#include <QQmlEngine>
+#include <qqml.h>
 
 // Forward declaration of the C++ class
-class LingmoLogger: public QObject {
+class LingmoLogger : public QObject {
     Q_OBJECT
-    public:
-        explicit LingmoLogger(QObject *parent = nullptr);
-        
+    Q_PROPERTY_AUTO(QString, loggingTag)
+    QML_NAMED_ELEMENT(LingmoLogger)
+    QML_SINGLETON
+
+private:
+    explicit LingmoLogger(QObject* parent = nullptr);
+
+    ~LingmoLogger();
+
+public:
+    SINGLETON(LingmoLogger)
+
+    Q_INVOKABLE void lAssert(const QString& msg);
+    Q_INVOKABLE void lError(const QString& msg);
+    Q_INVOKABLE void lWarn(const QString& msg);
+    Q_INVOKABLE void lInfo(const QString& msg);
+    Q_INVOKABLE void lDebug(const QString& msg);
+    Q_INVOKABLE void lVerbose(const QString& msg);
+
+    Q_INVOKABLE void lAseertTag(const QString& tag, const QString& msg);
+    Q_INVOKABLE void lErrorTag(const QString& tag, const QString& msg);
+    Q_INVOKABLE void lWarnTag(const QString& tag, const QString& msg);
+    Q_INVOKABLE void lInfoTag(const QString& tag, const QString& msg);
+    Q_INVOKABLE void lDebugTag(const QString& tag, const QString& msg);
+    Q_INVOKABLE void lVerboseTag(const QString& tag, const QString& msg);
+
+    // Call me in QML First!
+    Q_INVOKABLE void init();
 };
--- easylogger/qml_plugin/logger_plugin.cpp
+++ easylogger/qml_plugin/logger_plugin.cpp
@@ -1,16 +1,22 @@
-#include <QQmlExtensionPlugin>
 #include <QQmlEngine>
+#include <QQmlExtensionPlugin>
 
+#include "LingmoLogger.h"
 
-
-class QmlPlugins : public QQmlExtensionPlugin
-{
+class QmlPlugins : public QQmlExtensionPlugin {
     Q_OBJECT
     Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface")
 
 public:
-    void registerTypes(const char * uri) override {
-
+    void registerTypes(const char* uri) override
+    {
+        qmlRegisterSingletonType<LingmoLogger>(uri, 1, 0, "LingmoLogger",
+            [](QQmlEngine* engine, QJSEngine* scriptEngine) -> QObject* {
+                Q_UNUSED(scriptEngine)
+                QObject* instance = LingmoLogger::getInstance();
+                engine->setObjectOwnership(instance, QQmlEngine::CppOwnership);
+                return instance;
+            });
     }
 };
 
