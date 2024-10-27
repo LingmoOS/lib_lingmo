e3c9a9d5ca5858a26716cc30196d853f7452b205
Elysia <c.elysia@foxmail.com>
2024-10-27 23:30:35 +0800

    Added comments

--- screen/screen.h
+++ screen/screen.h
@@ -1,30 +1,37 @@
 #pragma once
 
 #include <QObject>
-#include <memory>
 #include <kscreen/getconfigoperation.h>
+#include <memory>
 
 #include "lingmoscreen_qmlplugins_export.h"
 
-#include "outputmodel.h"
 #include "confighandler.h"
-class LINGMOSCREEN_QMLPLUGINS_EXPORT Screen : public QObject
-{
+#include "outputmodel.h"
+class LINGMOSCREEN_QMLPLUGINS_EXPORT Screen : public QObject {
     Q_OBJECT
-    Q_PROPERTY(OutputModel *outputModel READ outputModel NOTIFY outputModelChanged)
+    Q_PROPERTY(OutputModel* outputModel READ outputModel NOTIFY outputModelChanged)
 
 public:
-    explicit Screen(QObject *parent = nullptr);
+    explicit Screen(QObject* parent = nullptr);
 
-    OutputModel *outputModel() const;
+    OutputModel* outputModel() const;
 
     void load();
     Q_INVOKABLE void save();
 
 private:
-    void configReady(KScreen::ConfigOperation *op);
+    void configReady(KScreen::ConfigOperation* op);
 
 Q_SIGNALS:
+    /**
+     * @brief outputModelChanged
+     * @details This signal is emitted when the output model is changed.
+     *    Also, the output model is ONLY available after this signal is emitted.
+     *    You will get a nullptr if this signal is not emitted, as the model
+     *    is being initialized.
+     * @since 1.0
+     */
     void outputModelChanged();
 
 private:
